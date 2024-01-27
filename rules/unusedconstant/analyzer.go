b86c31d04e24462060db7e1a1dc3d4a278f6301f
ceryspinch <cerys.pinch@gmail.com>
2024-01-27 13:36:11 +0000

    Add check for unused constant

--- rules/unusedconstant/analyzer.go
+++ rules/unusedconstant/analyzer.go
@@ -1,4 +1,4 @@
-package unusedvariable
+package unusedconstant
 
 import (
 	"go/ast"
@@ -11,8 +11,8 @@ import (
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name:     "unusedvariable",
-	Doc:      "Checks for variables that are declared at package level but never used",
+	Name:     "unusedconstant",
+	Doc:      "Checks for constants that are declared but never used",
 	Run:      run,
 	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
@@ -20,7 +20,7 @@ var Analyzer = &analysis.Analyzer{
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
-	// Go code will not compile if variables are declared but not used within functions,
+	// Go ode will not compile if variables are declared but not used within functions,
 	// so this code only checks for package level declarations in the form: 'var example string' or 'var example = "hello"'
 	// that pass compilation but are not then used within any functions throughout the code.
 	nodeFilter := []ast.Node{
@@ -29,7 +29,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 	inspector.Preorder(nodeFilter, func(node ast.Node) {
 		decl := node.(*ast.GenDecl)
-		if decl.Tok != token.VAR {
+		if decl.Tok != token.CONST {
 			return
 		}
 
@@ -40,14 +40,14 @@ func run(pass *analysis.Pass) (interface{}, error) {
 			}
 
 			for _, ident := range valSpec.Names {
-				varName := ident.Name
-				isUsed := checkUnusedVariable(pass, varName, spec.Pos())
+				constName := ident.Name
+				isUsed := checkUnusedConstant(pass, constName, spec.Pos())
 				if !isUsed {
 					pass.Reportf(
 						ident.Pos(),
-						(color.RedString("Variable %q has been declared but is not used, ", varName))+
+						(color.RedString("Constant %q has been declared but is not used, ", constName))+
 							color.BlueString("which means that is is redundant. ")+
-							color.GreenString("Delete the variable declaration if it is not needed or use it within a function."),
+							color.GreenString("Delete the constant declaration if it is not needed or use it within a function."),
 					)
 				}
 			}
@@ -57,22 +57,22 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
-func checkUnusedVariable(pass *analysis.Pass, varName string, declarationPos token.Pos) bool {
+func checkUnusedConstant(pass *analysis.Pass, constName string, declarationPos token.Pos) bool {
 	for _, file := range pass.Files {
 		found := false
 		ast.Inspect(file, func(n ast.Node) bool {
 			if ident, ok := n.(*ast.Ident); ok {
-				// Check if the identifier has the same name as the variable we're looking for and is not the original declaration
-				if ident.Name == varName && ident.Pos() != declarationPos {
+				// Check if the identifier has the same name as the constant we're looking for and is not the original declaration
+				if ident.Name == constName && ident.Pos() != declarationPos {
 					found = true
-					return false // Stop inspecting if the variable is found
+					return false // Stop inspecting if the constant is found
 				}
 			}
 			return true
 		})
 
 		if found {
-			// Stop searching in other files if the variable is found
+			// Stop searching in other files if the constant is found
 			return true
 		}
 	}
---
+++ testdata/src/unusedconstant/unusedconstant.go
@@ -0,0 +1,11 @@
+package unusedconstant
+
+import "fmt"
+
+const goodbyeMessage = "Bye!" // want "Constant \"goodbyeMessage\" has been declared but is not used, which means that is is redundant. Delete the constant declaration if it is not needed or use it within a function."
+
+const helloMessage = "Hi!"
+
+func helloWorld() {
+	fmt.Print(helloMessage)
+}
